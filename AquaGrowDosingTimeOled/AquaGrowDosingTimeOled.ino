1e5c0fa33e414a7f676dab4b9a15d000a8cc9f8d
MajorMadness <mstravendev@gmail.com>
2015-02-11 12:18:15 +0100

    Hinweise zur verwendung meiner Scripte in anderen Projekten hinzugefügt

---
+++ AquaGrowDosingTimeOled/AquaGrowDosingTimeOled.ino
@@ -0,0 +1,139 @@
+/*
+  AquaGrow Programmcode zur Steuerung von Aquarien
+  Copyright (C)2013-2015 AquaGrow - Moritz Stapelfeldt. All right reserved
+  
+  Der Code wurde entwickelt um Aquarien zu automatisieren und zu steuern. Er ist frei zugänglich und unterliegt dem Gedanken des OpenSource. Jeder kann ihn kostenfrei verwenden, kopieren, ganz oder teilweise solange die verwendeten Codezeilen als Fremdcode Kenntlich gemacht wurden und ein Link zum Orginal und/oder zur Webseite http://www.Aqua-Grow.de erhalten bleibt.
+  
+  Ich, als Urheber des Quellcodes übernehme keine Garantie auf die funktion des Quellcodes im ganzen oder teilen. Für evt Bugfixes und Updates besuche bitte http://www.Aqua-Grow.de und/oder https://github.com/majormadness
+  
+  Wenn veränderungen, modifikationen und/oder verbesserungen am Code vorgenommen werden bitte Ich um mitteilung unter info@aqua-grow.de
+
+  This Software is free software; you can redistribute it and/or modify it under the terms of the CC BY-NC-SA 3.0 license.
+
+  Commercial use of this Software requires you to buy a license that will allow commercial use. This includes using the Software, modified or not, as a tool to sell products.
+
+  The license applies to all part of the Software including the examples and tools supplied with the Software.
+*/
+
+#include <Wire.h>
+#include <RTClib.h>
+#include <Timer.h>
+#include <OneWire.h>
+#include <MemoryFree.h>
+#include <Keypad.h>  
+#include <Keypad_I2C.h>  
+#include <EEPROM.h>
+#include "writeAnything.h" 
+#include "structs.h"
+#include "config.h"
+
+#include <MicroLCD.h>
+LCD_SSD1306 lcd; 
+
+Timer t;
+RTC_DS1307 rtc;
+OneWire  ds(PIN_TEMP);
+
+boolean pumpReset=false;
+
+
+boolean manualRelay=false;
+unsigned long last_print = 0;
+String inputString = "";
+boolean stringComplete = false;
+float temperatur;
+
+
+const byte ROWS = 4; //four rows
+const byte COLS = 4; //four columns
+//define the cymbols on the buttons of the keypads
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[ROWS] = {0, 1, 2, 3}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {4, 5, 6, 7}; //connect to the column pinouts of the keypad
+
+char holdKey;
+unsigned long t_hold;
+
+//initialize an instance of class NewKeypad
+Keypad_I2C keypad = Keypad_I2C( makeKeymap(keys), rowPins, colPins, ROWS, COLS, KEYPADI2C );
+
+
+void setup() {
+
+  Wire.begin();
+  rtc.begin();
+  keypad.begin( );  
+  keypad.addEventListener(keypadEvent); // Add an event listener for this keypad
+  
+  
+  Serial.begin(9600);
+
+  lcd.begin();
+    lcd.setCursor(0,0); 
+    lcd.print(F("AquaGrow DosingTime"));
+
+  for (int n=6;n<=12;n++){
+    pinMode(n, OUTPUT);
+  }
+  for (int i=0;i<4;i++){
+    pinMode(relayPIN[i], OUTPUT);
+  }
+  inputString.reserve(20);
+  
+}
+
+void loop() {
+  rtc.now();
+  t.update();
+  char key = keypad.getKey();
+  
+  
+    if(!manualRelay){
+      setRelay();
+    }
+  // print the string when a newline arrives:
+  if (stringComplete) {
+    serialHandler(inputString);
+    inputString = "";
+    stringComplete = false;
+  }
+
+  if (millis() - last_print > 1000) {
+    last_print = millis();
+    
+    temperatur = getTemp();
+    Serial.println(retTime());
+//    Serial.println(freeMemory());
+    
+    setDosing();
+    
+    printTime();
+    printTemp(temperatur);
+
+  }            
+}
+
+float getTemp(){
+  byte present = 0;
+  byte data[12];
+  byte addr[8];
+  ds.search(addr);
+      
+  ds.reset();
+  ds.select(addr);
+  ds.write(0x44, 1);
+  present = ds.reset();
+  ds.select(addr);    
+  ds.write(0xBE); 
+  for (byte i = 0; i < 9; i++) {
+    data[i] = ds.read();
+  }
+  int16_t raw = (data[1] << 8) | data[0];
+  
+  return (float)raw / 16.0; //celsius;
+}
---
+++ AquaGrowDosingTimeOled/config.h
@@ -0,0 +1,52 @@
+#define PUMPCOUNTS 7      // Number Pumps
+#define RELAYCHANNELS 4
+#define RELAYTIMES 6
+#define KEYPADI2C 0x20
+#define LCD_ADR 0x27
+
+
+  // PIN MAPPING NICHT ÄNDERN!!!
+  #define PIN_TEMP A1   // Temperatur
+  #define RELAY1 2
+  #define RELAY2 3
+  #define RELAY3 4
+  #define RELAY4 5
+  #define DOSE7 6      // Dosierpumpe 
+  #define DOSE6 7       // Dosierpumpe 
+  #define DOSE5 8      // Dosierpumpe 
+  #define DOSE4 9     // Dosierpumpe 
+  #define DOSE3 10      // Dosierpumpe 
+  #define DOSE2 11     // Dosierpumpe 
+  #define DOSE1 12     // Dosierpumpe 
+
+// Größe Vorratsflaschen Dünger in ml
+const int dose_val = 1000;
+
+// Neu zuordnung der Pins für Dosierung
+const byte dosingPins[PUMPCOUNTS]={DOSE1,DOSE2,DOSE3,DOSE4,DOSE5,DOSE6,DOSE7};
+// Milliliter per minute in Reihenfolge wie Pumpen oben Achtung Maximum 255ml/Minute
+const byte dosingMlMin[PUMPCOUNTS]={60,60,60,60,60,60,60};
+
+// Pumpen Einstellungen
+// Uhrzeit, Aktive, Name, Pin Adresse (von array oben 0 ist erste Pumpe "Dose1") Milliliter pro Dosierung
+const PUMP dosing[] = {  
+{get_ts(10,0,0),1,"D1",0,5},
+{get_ts(11,0,0),1,"D2",1,6},
+{get_ts(11,25,0),1,"D3",2,3},
+{get_ts(11,26,0),1,"D4",3,3},
+{get_ts(11,23,0),1,"D3",4,3},
+{get_ts(11,28,0),1,"D1",5,5},
+{get_ts(11,28,0),1,"D1",6,5},
+};
+
+// Wird verwendet um zu sehen ob schon dosiert wurde. Nicht Ändern!!!
+boolean dosingState[sizeof dosing / sizeof dosing[0]];
+
+const byte relayPIN[]= {RELAY1,RELAY2,RELAY3,RELAY4};
+RELAY relays[RELAYCHANNELS][RELAYTIMES] = { 
+  {{get_ts(0,0,0),1},{get_ts(17,10,0),0},{get_ts(17,15,0),1},{get_ts(17,20,0),0},{get_ts(17,25,0),1},{get_ts(17,30,0),0}},
+  {{get_ts(0,0,0),0},{get_ts(10,0,0),1},{get_ts(11,30,0),0},{get_ts(19,0,0),1},{get_ts(20,0,0),0},{get_ts(21,30,0),0}},
+  {{get_ts(0,0,0),0},{get_ts(10,0,0),1},{get_ts(11,30,0),0},{get_ts(19,0,0),1},{get_ts(20,0,0),0},{get_ts(21,30,0),0}},
+  {{get_ts(0,0,0),0},{get_ts(10,0,0),1},{get_ts(11,30,0),0},{get_ts(19,0,0),1},{get_ts(20,0,0),0},{get_ts(21,30,0),0}}
+};
+
